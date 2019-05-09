a8fb79d8c43d49599fd459198794b6566c2f428b
Wes Turner <wturnerharris@gmail.com>
2019-05-08 22:44:53 -0400

    terminus 2.0.1

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -1,8 +1,8 @@
 class Terminus < Formula
   desc "Standalone utility for performing operations on the Pantheon Platform"
   homepage "https://github.com/pantheon-systems/terminus"
-  url "https://github.com/pantheon-systems/terminus/archive/1.9.0.tar.gz"
-  sha256 "4e315e950affbf0c95e7b1225b80151c85ff8ae04b2aad053c2adb5c30380f93"
+  url "https://github.com/pantheon-systems/terminus/archive/2.0.1.tar.gz"
+  sha256 "69d195f6cc5bfffa5d0bec52807765d75a274203e5d1de9f38cac02324529bfe"
 
   depends_on "Composer"
 
